b5b59ab422c6df8323afceac4e40274f56c6c965
toriz7 <30500848+toriz7@users.noreply.github.com>
2018-12-09 19:51:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express=require('express');
 let app=express();
+var session=require('express-session');
 
 var jq = require("jquery");
+var bodyParser=require("body-parser");
 
+app.use(session({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 60*2000 }
+  //using secure flag means that the cookie will be set on Https only
+}))
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/template',express.static('./template/'));
 app.use('/jquery', express.static('./template/js/node_modules/jquery/dist/'));
 
@@ -12,27 +22,17 @@ app.set('view engine','ejs');//어떤 형식의 템플릿을 사용할 것인지
 app.locals.pretty=true;
 
 
-let bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
 
 let OrientDB=require('orientjs');
 let server=OrientDB({
   host:'localhost',
   port:2424,//기본 포트
   username:'root',
-  password:'****'
+  password:'**********'
 });
 let db=server.use('gettingStarted');
 console.log('Using Database:'+ db.name);
 
-app.get('/',function(req,res){
-  res.render('index.ejs');
-});
-
-app.get('/signup',function(req,res){
-  //let title='학생 성적표 등록하기';
-  res.render('signup.ejs');
-});
 app.post('/signup',function(req,res){
   let pname=req.body.personName;
   let pdate=req.body.personBirthDate;
@@ -43,7 +43,7 @@ app.post('/signup',function(req,res){
   let ppwd=req.body.personPwd;
   let ppwd2=req.body.personPwd2;
 
-let sql='insert into personInfo (name,birthDate,bloodType,email,coinPocket,id,pwd)'+
+var sql='insert into personInfo (name,birthDate,bloodType,email,coinPocket,id,pwd)'+
   'values(:name,:birthDate,:bloodType,:email,:coinPocket,:id,:pwd)';
     db.query(sql,{
       params:{
@@ -60,8 +60,57 @@ let sql='insert into personInfo (name,birthDate,bloodType,email,coinPocket,id,pw
   res.redirect('/');
 })
 
+app.get('/login',function(req,res){
+  res.render('login.ejs');
+});
 
+app.post('/loginverify',function(req,res){
+  var id=req.body.id;
+  var password=req.body.password;
+  var sql="select id,pwd,name from personInfo where id='"+id+"'and pwd='"+password+"'";
 
+  db.query(sql).then(function(results){
+
+    //console.log(results[0].name);
+    if((id==results[0].id) && (password==results[0].pwd)){
+
+    req.session.user={
+        id: results[0].id,
+        name:results[0].name
+    };
+    req.session.save(() => {
+      //var user=req.session.user;
+      res.render('index.ejs',{name:req.session.user.name})
+    });
+
+    //console.log(req.session.user);
+    }
+    else {
+      res.redirect('/')
+    }
+  });
+
+})
+
+app.get('/',function(req,res){
+    if(req.session.user){
+    var user=req.session.user;
+    //console.log(user);
+    res.render('index.ejs',{name:req.session.user.name});
+    }
+    else{
+    res.render('index.ejs',{name:null});
+    }
+});
+app.get('/logout',function (req,res) {
+    req.session.user= null;
+    //왜 req.session.destroy() 가 3.0 에서는 안 될까?
+    res.redirect('/');
+})
+app.get('/signup',function(req,res){
+  //let title='학생 성적표 등록하기';
+  res.render('signup.ejs');
+});
 app.listen(3000,function(req,res){
   console.log('port 3000 connected!');
 });
